7d2085b56edd0d745d4a298783448f25935ee08b
adriNavar <adridonorione@gmail.com>
2025-07-25 09:58:43 -0300

    Correccion RR1

--- apps/usuarios/forms.py
+++ apps/usuarios/forms.py
@@ -2,10 +2,10 @@
 
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from .models import Usuario # Tu modelo de usuario personalizado
+from .models import Usuario 
 
 class RegistroForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta): # Opcional: heredar la Meta de UserCreationForm
+    class Meta(UserCreationForm.Meta): # heredar la Meta de UserCreationForm(esto ya es propio de django)
         model = Usuario
         fields = [
             'first_name',
@@ -18,13 +18,12 @@ class RegistroForm(UserCreationForm):
       
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # Opcional: Personalizar labels o widgets si lo necesitas,
-        # pero para los campos que ya existen, Django toma el label del modelo.
-        self.fields['email'].required = True # Asegura que el email sea requerido, aunque tu modelo ya lo dice
+       
+        self.fields['email'].required = True 
         self.fields['first_name'].required = True
         self.fields['last_name'].required = True
 
-        # Personalizar labels si quieres que sean diferentes a los del modelo
+        # aca cambiamos despues los label si queremos
         self.fields['username'].label = 'Nombre de Usuario'
         self.fields['email'].label = 'Correo Electrónico'
         self.fields['first_name'].label = 'Nombre'
